d41334ceaa6f0b1daf0c5169515c10f6dbc534e4
Nast1aR <135260975+Nast1aR@users.noreply.github.com>
2024-03-28 13:34:33 +0200

    telegram bot was greated

---
+++ cmd/golangChatbot.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	// TeleToken bot
+	TeleToken = os.Getenv("TELE_TOKEN")
+)
+
+// golangChatbotCmd represents the golangChatbot command
+var golangChatbotCmd = &cobra.Command{
+	Use:   "golangChatbot",
+	Aliases: []string{"start"}, 
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Printf("golangChatbot %s started")
+
+		golangChatbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		golangChatbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			return nil
+		})
+
+		golangChatbot.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(golangChatbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// golangChatbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// golangChatbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+const appVersion = "1.0.0"
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/Nast1aR/golang_chatbot/cmd"
+
+func main() {
+	cmd.Execute()
+}
